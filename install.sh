3b43b20d884787762ba3ae5b8b95be45536b3e69
Ponson39 <65836189+Ponson39@users.noreply.github.com>
2021-02-02 18:10:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git
 apt-get source debian-installer
 cd live-build-config
 
-cat > config/package-lists/kali.list.chroot << EOF
+cat > kali-config/variant-default/package-lists/kali.list.chroot << EOF
   kali-linux-core
   xfce4
   light-dm
@@ -21,5 +21,27 @@ cat > config/package-lists/kali.list.chroot << EOF
   xorg
 EOF
 
-mkdir -p config/includes.chroot/usr/share/wallpapers/kali/contents/images
-wget 
+mkdir -p kali-config/common/includes.chroot/usr/share/wallpapers/kali/contents/images
+wget https://i.imgur.com/LEhta3r.jpg
+mv LEhta3r.jpg kali-config/common/includes.chroot/usr/share/wallpapers/kali/contents/images/logo.jpg
+
+mkdir -p kali-config/common/includes.chroot/usr/share/themes/
+git clone https://github.com/vinceliuice/Matcha-gtk-theme.git
+sh Matcha-gtk-theme/install.sh -d kali-config/common/includes.chroot/usr/share/themes/ -t aliz
+
+cat > kali-config/common/hooks/xfce.chroot << EOF 
+  #!/bin/bash
+  systemctl enable ligthdm.service
+  systemctl start lightdm.service
+  xconf-query -c xsettings -p /Net/ThemeName -s "Matcha-dark-aliz"
+EOF
+
+mkdir -p kali-config/common/debian-installer/
+cp ../debian-installer-*/build/preseed.cfg config/debian-installer/
+sed -i 's/make-user boolean false/make-user boolean true/' config/debian-installer/preseed.cfg
+echo "d-i passwd/root-login boolean false" >> config/debian-installer/preseed.cfg
+
+mkdir kali-config/common/packages.chroot/
+mv ../paquetes/ kali-config/common/packages.chroot/
+
+./build.sh -v
