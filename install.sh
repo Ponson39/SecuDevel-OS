2ebcdb9f4b5cb80fc73ed14b4e2fd0282745ce46
Ponson39 <65836189+Ponson39@users.noreply.github.com>
2021-02-03 15:07:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git
 
 cd live-build-config
 
-cat > config/variant-default/package-lists/kali.list.chroot << EOF
+cat > kali-config/variant-default/package-lists/kali.list.chroot << EOF
   kali-root-login
   kali-menu
   kali-debtags
@@ -27,19 +27,19 @@ EOF
 
 echo Añadiendo imagen a Wallpapers
 
-mkdir -p config/common/includes.chroot/usr/share/wallpapers/kali/contents/images
+mkdir -p kali-config/common/includes.chroot/usr/share/wallpapers/kali/contents/images
 wget https://i.imgur.com/LEhta3r.jpg
-mv LEhta3r.jpg config/common/includes.chroot/usr/share/wallpapers/kali/contents/images/logo.jpg
+mv LEhta3r.jpg kali-config/common/includes.chroot/usr/share/wallpapers/kali/contents/images/logo.jpg
 
 echo Clonando tema GTK3
-mkdir -p config/common/includes.chroot/usr/share/themes/
+mkdir -p kali-config/common/includes.chroot/usr/share/themes/
 git clone https://github.com/vinceliuice/Matcha-gtk-theme.git
 cd Matcha-gtk-theme
 chmod a+x install.sh
-./install.sh -d ../config/common/includes.chroot/usr/share/themes/ -t aliz
+./install.sh -d ../kali-config/common/includes.chroot/usr/share/themes/ -t aliz
 cd ..
 
-cat > config/common/hooks/xfce.chroot << EOF 
+cat > kali-config/common/hooks/xfce.chroot << EOF 
   #!/bin/bash
   systemctl enable ligthdm.service
   systemctl start lightdm.service
@@ -47,10 +47,10 @@ cat > config/common/hooks/xfce.chroot << EOF
   xfconf-query -c xfwm4 -p /general/theme -s "Matcha-dark-aliz"
 EOF
 
-mkdir -p config/common/debian-installer/
-wget https://gitlab.com/kalilinux/recipes/kali-preseed-examples/-/raw/master/kali-linux-full-unattended.preseed -O config/common/debian-installer/preseed.cfg
-sed -i 's/make-user boolean false/make-user boolean true/' config/debian-installer/preseed.cfg
-echo "d-i passwd/root-login boolean false" >> config/debian-installer/preseed.cfg
+mkdir -p kali-config/common/debian-installer/
+wget https://gitlab.com/kalilinux/recipes/kali-preseed-examples/-/raw/master/kali-linux-full-unattended.preseed -O kali-config/common/debian-installer/preseed.cfg
+sed -i 's/make-user boolean false/make-user boolean true/' kali-config/common/debian-installer/preseed.cfg
+echo "d-i passwd/root-login boolean false" >> kali-config/common/debian-installer/preseed.cfg
 
 mkdir -p paquetes
 touch paquetes/prueba.txt
@@ -63,9 +63,9 @@ wget -i prueba.txt
 
 cd ..
 
-mkdir config/common/packages.chroot/
-mv paquetes/* config/common/packages.chroot/
+mkdir kali-config/common/packages.chroot/
+mv paquetes/* kali-config/common/packages.chroot/
 
 echo se va a construir el paquete
 
-./build.sh -v --dist chaquen_OS
+./build.sh -v
